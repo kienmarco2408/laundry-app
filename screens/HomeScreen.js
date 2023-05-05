2facad9feeaf97bdbfbe537ecabb39163d37870c
KienTran <kienmarco999@gmail.com>
2023-05-05 21:26:38 +0700

    update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,13 +3,18 @@ import {
   Image,
   Pressable,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
-import { MaterialIcons } from "@expo/vector-icons";
+import { Feather, MaterialIcons } from "@expo/vector-icons";
+import Carousel from "../components/Carousel";
+import Services from "../components/Services";
+import DressItem from "../components/DressItem";
 
 const HomeScreen = () => {
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(
@@ -78,8 +83,59 @@ const HomeScreen = () => {
       }
     }
   };
+  const services = [
+    {
+      id: "0",
+      image: "https://cdn-icons-png.flaticon.com/128/4643/4643574.png",
+      name: "shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "11",
+      image: "https://cdn-icons-png.flaticon.com/128/892/892458.png",
+      name: "T-shirt",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "12",
+      image: "https://cdn-icons-png.flaticon.com/128/9609/9609161.png",
+      name: "dresses",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "13",
+      image: "https://cdn-icons-png.flaticon.com/128/599/599388.png",
+      name: "jeans",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "14",
+      image: "https://cdn-icons-png.flaticon.com/128/9431/9431166.png",
+      name: "Sweater",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "15",
+      image: "https://cdn-icons-png.flaticon.com/128/3345/3345397.png",
+      name: "shorts",
+      quantity: 0,
+      price: 10,
+    },
+    {
+      id: "16",
+      image: "https://cdn-icons-png.flaticon.com/128/293/293241.png",
+      name: "Sleeveless",
+      quantity: 0,
+      price: 10,
+    },
+  ];
   return (
-    <SafeAreaView>
+    <ScrollView style={{ backgroundColor: "#F0F0F0", flex: 1, marginTop: 50 }}>
       <View style={{ flexDirection: "row", alignItems: "center" }}>
         <MaterialIcons name="location-on" size={24} color="#fd5c63" />
         <View>
@@ -95,7 +151,30 @@ const HomeScreen = () => {
           />
         </Pressable>
       </View>
-    </SafeAreaView>
+      <View
+        style={{
+          padding: 10,
+          margin: 10,
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          borderWidth: 0.8,
+          borderColor: "#C0C0C0",
+          borderRadius: 7,
+        }}
+      >
+        <TextInput placeholder="Search for items or More" />
+        <Feather name="search" size={24} color="#fd5c63" />
+      </View>
+      {/* Image Casuosel */}
+      <Carousel />
+      {/* Services Component */}
+      <Services />
+      {/* Render all the Products */}
+      {services.map((item, index) => (
+        <DressItem item={item} key={index} />
+      ))}
+    </ScrollView>
   );
 };
 
